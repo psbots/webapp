92cbe384cd257a913d1fb6625766e951f5d09c4f
Varghese Thomas <varghesethomase@gmail.com>
2017-11-12 00:49:27 +0530

    Step one and two components

---
+++ app/containers/CategorySelection/index.js
@@ -0,0 +1,68 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
+import StepOne from 'containers/Steps/StepOne';
+// import { changeUsername } from './actions';
+// import { makeSelectUsername } from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  /**
+   * when initial state username is not null, submit the form to load repos
+   */
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <article>
+        <Helmet>
+          <title>Home Page</title>
+          <meta name="description" content="A React.js Boilerplate application homepage" />
+        </Helmet>
+        <StepOne />
+      </article>
+    );
+  }
+}
+
+HomePage.propTypes = {
+};
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    simply: () => dispatch(),
+    // onChangeUsername: (evt) => dispatch(changeUsername(evt.target.value)),
+    // onSubmitForm: (evt) => {
+    //   if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+    //   dispatch(loadRepos());
+    // },
+  };
+}
+
+const mapStateToProps = createStructuredSelector({
+});
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'home', reducer });
+const withSaga = injectSaga({ key: 'home', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(HomePage);
