334906c00d58f2f326b40db327604587b72bc087
Varghese Thomas <varghesethomase@gmail.com>
2017-11-12 10:31:44 +0530

    screen selector changes to accomodate state change

--- app/components/ScreenSelector/index.js
+++ app/components/ScreenSelector/index.js
@@ -8,29 +8,33 @@ export default class ScreenSelector extends Component {
     selected: false,
   };
   render() {
+    const { id, toggleImageSelection } = this.props;
     let classnames = 'ScreenSelectorCard';
     if (this.state.selected) {
       classnames = 'ScreenSelectorCard ScreenSelected';
     }
     return (
-      <div>
-        <Card
-          onClick={() => this.setState({
+      <Card
+        onClick={() => {
+          toggleImageSelection(id);
+          this.setState({
             selected: !this.state.selected,
-          })}
-          className={classnames}
-          inverse
-        >
-          <CardImg top width="100%" src={this.props.imageUrl} alt="Card image cap" />
-          {
-            this.state.selected ? <CardImgOverlay className="selectedOverlayLayer"><div className="overlay-container"><div className="selected-icon"><i className="ion-checkmark-circled mr-1"></i></div></div></CardImgOverlay> : null
-          }
-        </Card>
-      </div>
+          });
+        }}
+        className={classnames}
+        inverse
+      >
+        <CardImg top width="100%" src={this.props.imageUrl} alt="Card image cap" />
+        {
+          this.state.selected ? <CardImgOverlay className="selectedOverlayLayer"><div className="overlay-container"><div className="selected-icon"><i className="ion-checkmark-circled mr-1"></i></div></div></CardImgOverlay> : null
+        }
+      </Card>
     );
   }
 }
 
 ScreenSelector.propTypes = {
   imageUrl: PropTypes.String,
+  id: PropTypes.String,
+  toggleImageSelection: PropTypes.func,
 };
